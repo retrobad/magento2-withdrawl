9826ac697a6b7fc97579904b746a9877b71fea03
codedge <post@codedge.de>
2026-05-13 17:45:31 +0200

    Adjust translations, modify fields and email templates

--- registration.php
+++ registration.php
@@ -3,5 +3,5 @@ use Magento\Framework\Component\ComponentRegistrar;
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'Zwernemann_Withdrawal',
-    __DIR__
+    __DIR__ // testing: BP . '/vendor/zwernemann/module-withdrawal'
 );
